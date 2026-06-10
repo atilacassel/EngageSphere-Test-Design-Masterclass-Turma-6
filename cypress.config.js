f5f065674e8ea5877cbd04647a1ce0da999d0de9
Atila Cassel <atilacassel@gmail.com>
2026-06-10 14:44:36 -0300

    chore(config): separate frontend baseUrl and backend apiUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
+
+    env: {
+      apiUrl: 'http://localhost:3001',
+    },
   },
 })
--- cypress/e2e/api/customers.cy.js
+++ cypress/e2e/api/customers.cy.js
@@ -1,35 +1,32 @@
 describe('GET /customers', () => {
+  it('should return a list of customers', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrl')}/customers`,
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('customers')
+      expect(response.body.customers).to.be.an('array')
+      expect(response.body.customers.length).to.be.greaterThan(0)
+      expect(response.body.customers[0]).to.have.property('id')
 
-    it('should return a list of customers', () => {
-        
-        cy.request('GET', '/customers')
-        .then((response) => {
-
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('customers')
-            expect(response.body.customers).to.be.an('array')
-            expect(response.body.customers.length).to.be.greaterThan(0)
-            expect(response.body.customers[0]).to.have.property('id')
-
-            expect(response.body).to.have.property('pageInfo')
-            expect(response.body.pageInfo).to.have.property('currentPage')
-            expect(response.body.pageInfo).to.have.property('totalPages')
-            expect(response.body.pageInfo).to.have.property('totalCustomers')
-        })
+      expect(response.body).to.have.property('pageInfo')
+      expect(response.body.pageInfo).to.have.property('currentPage')
+      expect(response.body.pageInfo).to.have.property('totalPages')
+      expect(response.body.pageInfo).to.have.property('totalCustomers')
     })
+  })
 
-    it.only('should return 400 for invalid page parameter', () => {
-
-        cy.request({
-            method: 'GET',
-            url: '/customers?page=-1',
-            failOnStatusCode: false
-        })
-        .then((response) => {
-
-            expect(response.status).to.eq(400)
-            expect(response.body.error).to.eq('Invalid page or limit. Both must be positive numbers.')
-
-        })
+  it('should return 400 for invalid page parameter', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrl')}/customers?page=-1`,
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+      expect(response.body.error).to.eq(
+        'Invalid page or limit. Both must be positive numbers.',
+      )
     })
+  })
 })
