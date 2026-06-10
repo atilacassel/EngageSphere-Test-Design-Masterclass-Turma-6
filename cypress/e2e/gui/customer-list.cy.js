8b2ffb8cae010eb2136880febf01b01e852724bf
Atila Cassel <atilacassel@gmail.com>
2026-06-10 15:46:06 -0300

    test(gui) validate selected filters when returns from details page

---
+++ cypress/e2e/gui/customer-list.cy.js
@@ -0,0 +1,15 @@
+describe('EngageSphere - Customer List', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.contains('button', 'Accept').click()
+  })
+  it('should keep the selected filters when returns from the details page', () => {
+    cy.get('[data-testid="size-filter"]').select('Small')
+    cy.get('[data-testid="industry-filter"]').select('Finance')
+    cy.contains('button', 'View').click()
+    cy.contains('button', 'Back').click()
+
+    cy.get('[data-testid="size-filter"]').should('have.value', 'Small')
+    cy.get('[data-testid="industry-filter"]').should('have.value', 'Finance')
+  })
+})
