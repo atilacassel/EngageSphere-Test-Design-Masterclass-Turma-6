51177299a96761c1c927f5a2a9e875f2039190d4
Atila Cassel <atilacassel@gmail.com>
2026-06-17 17:18:09 -0300

    test(gui): validate return to the customer list

--- cypress/e2e/gui/customer-list.cy.js
+++ cypress/e2e/gui/customer-list.cy.js
@@ -4,7 +4,7 @@ describe('EngageSphere - Customer List', () => {
     cy.visit('/')
     cy.contains('button', 'Accept').click()
   })
-  
+
   it('should keep the selected filters when returns from the details page', () => {
     cy.get('[data-testid="size-filter"]').select('Small')
     cy.get('[data-testid="industry-filter"]').select('HR')
@@ -13,5 +13,12 @@ describe('EngageSphere - Customer List', () => {
 
     cy.get('[data-testid="size-filter"]').should('have.value', 'Small')
     cy.get('[data-testid="industry-filter"]').should('have.value', 'HR')
+  })
+
+  it('should return to the customer list by clicking the Back button', () => {
+    cy.get(':nth-child(1) > :nth-child(6) > strong > button').click()
+    cy.contains('button', 'Back').click()
+
+    cy.get('[data-testid="table"]').should('be.visible')
   })
 })
