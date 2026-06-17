4f20d56d47b9cd8edfbf06a269c1b0ec9365f548
Atila Cassel <atilacassel@gmail.com>
2026-06-17 16:44:42 -0300

    refactor(api): apply object destructuring to response objects

--- cypress/e2e/api/customers.cy.js
+++ cypress/e2e/api/customers.cy.js
@@ -6,17 +6,17 @@ describe('GET /customers', () => {
   })
 
   it('should return a list of customers', () => {
-    cy.request('GET', `${apiUrl}/customers`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.have.property('customers')
-      expect(response.body.customers).to.be.an('array')
-      expect(response.body.customers.length).to.be.greaterThan(0)
-      expect(response.body.customers[0]).to.have.property('id')
+    cy.request('GET', `${apiUrl}/customers`).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body).to.have.property('customers')
+      expect(body.customers).to.be.an('array')
+      expect(body.customers.length).to.be.greaterThan(0)
+      expect(body.customers[0]).to.have.property('id')
 
-      expect(response.body).to.have.property('pageInfo')
-      expect(response.body.pageInfo).to.have.property('currentPage')
-      expect(response.body.pageInfo).to.have.property('totalPages')
-      expect(response.body.pageInfo).to.have.property('totalCustomers')
+      expect(body).to.have.property('pageInfo')
+      expect(body.pageInfo).to.have.property('currentPage')
+      expect(body.pageInfo).to.have.property('totalPages')
+      expect(body.pageInfo).to.have.property('totalCustomers')
     })
   })
 
@@ -25,9 +25,9 @@ describe('GET /customers', () => {
       method: 'GET',
       url: `${apiUrl}/customers?page=-1`,
       failOnStatusCode: false,
-    }).then((response) => {
-      expect(response.status).to.eq(400)
-      expect(response.body.error).to.eq(
+    }).then(({ status, body }) => {
+      expect(status).to.eq(400)
+      expect(body.error).to.eq(
         'Invalid page or limit. Both must be positive numbers.',
       )
     })
