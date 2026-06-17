c1e241e838f7453c10897275732fcb1ae7f70767
Atila Cassel <atilacassel@gmail.com>
2026-06-17 16:58:18 -0300

    test(gui): update company selection and remove unused cypress folders

--- cypress/e2e/gui/customer-list.cy.js
+++ cypress/e2e/gui/customer-list.cy.js
@@ -1,15 +1,17 @@
 describe('EngageSphere - Customer List', () => {
+
   beforeEach(() => {
     cy.visit('/')
     cy.contains('button', 'Accept').click()
   })
+  
   it('should keep the selected filters when returns from the details page', () => {
     cy.get('[data-testid="size-filter"]').select('Small')
-    cy.get('[data-testid="industry-filter"]').select('Finance')
+    cy.get('[data-testid="industry-filter"]').select('HR')
     cy.contains('button', 'View').click()
     cy.contains('button', 'Back').click()
 
     cy.get('[data-testid="size-filter"]').should('have.value', 'Small')
-    cy.get('[data-testid="industry-filter"]').should('have.value', 'Finance')
+    cy.get('[data-testid="industry-filter"]').should('have.value', 'HR')
   })
 })
