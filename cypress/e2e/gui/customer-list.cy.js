cac71b47ac8686ef4dd9c4801463ef167cc3178f
Atila Cassel <atilacassel@gmail.com>
2026-06-24 15:51:43 -0300

    test(gui): mock customers API to fix flaky filter test

--- cypress/e2e/gui/customer-list.cy.js
+++ cypress/e2e/gui/customer-list.cy.js
@@ -1,15 +1,43 @@
 describe('EngageSphere - Customer List', () => {
-
   beforeEach(() => {
     cy.setCookie('cookieConsent', 'accepted')
     cy.visit('/')
   })
 
   it('should keep the selected filters when returns from the details page', () => {
+    cy.intercept('GET', '**/customers*', {
+      statusCode: 200,
+      body: {
+        customers: [
+          {
+            id: 999,
+            name: 'Company Test LTDA',
+            employees: 50,
+            industry: 'HR',
+            contactInfo: null,
+            address: {
+              street: 'Rua Fictícia, 123',
+              city: 'Florianópolis',
+              state: 'Santa Catarina',
+              zipCode: '88000-000',
+              country: 'Brazil',
+            },
+            size: 'Small',
+          },
+        ],
+        pageInfo: {
+          currentPage: 1,
+          totalPages: 1,
+          totalCustomers: 1,
+        },
+      },
+    }).as('getFilteredCustomers')
+
+    cy.wait('@getFilteredCustomers')
+
     cy.get('[data-testid="size-filter"]').select('Small')
     cy.get('[data-testid="industry-filter"]').select('HR')
     cy.contains('button', 'View').click()
-
     cy.contains('button', 'Back').click()
 
     cy.get('[data-testid="size-filter"]').should('have.value', 'Small')
